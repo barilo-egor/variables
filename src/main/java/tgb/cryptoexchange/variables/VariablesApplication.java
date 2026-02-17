52083d5774cb8deb50f90f8920fb27b1539556d5
GrewGh <grew.gh@mail.ru>
2026-02-17 20:46:44 +0300

    init

---
+++ src/main/java/tgb/cryptoexchange/variables/VariablesApplication.java
@@ -0,0 +1,13 @@
+package tgb.cryptoexchange.variables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VariablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VariablesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tgb/cryptoexchange/variables/VariablesApplicationTests.java
@@ -0,0 +1,13 @@
+package tgb.cryptoexchange.variables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VariablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
